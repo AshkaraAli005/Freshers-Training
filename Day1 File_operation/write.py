59d34d6c873d5ca2091393fa06ffcbae3ae163cf
AshkaraAli005 <94950485+AshkaraAli005@users.noreply.github.com>
2023-07-03 17:36:22 +0530

    'alters'

--- Day1 File_operation/write.py
+++ Day1 File_operation/write.py
@@ -1,13 +1,14 @@
 import os.path
 filePath = input("File Path : ") #Getting path as input
+print("Enter 'END' to terminate ...")
 is_exists = os.path.exists(filePath)
 
-while (True):
-        fileText = input()
+while (True):#checking whether text is not empty
+        fileText = input()  #Getting the data as input
         file = open(filePath , 'a')
          
-        if (fileText.lower() == 'end'):
+        if (fileText.lower() == 'end'): #To stop entering the text type END
             break
-        file.write(fileText+'\n')
+        file.write(fileText+'\n') 
         file.close()
 print(f'Changes are made in file :{str(filePath)}')
